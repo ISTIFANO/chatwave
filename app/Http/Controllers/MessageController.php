1418391a470190d455d2a05e5bba24fb61a4a2f1
istifano <elamiriamir7@gmail.com>
2025-03-14 00:17:45 +0100

    afficher pour l utilisateur list des utilisateurs qui sont disponible dans l application

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Container\Attributes\Auth;
 
 class MessageController extends Controller
 {
-    //
+    public function index(){
+        $users = User::where("id",'!=',Auth::id())->get();
+        
+return view("users",compact(["users"=>$users]));
+    }
 }
