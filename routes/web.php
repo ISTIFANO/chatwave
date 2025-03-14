f0969d75522c758b8c22d043123d08e98fc81b08
unknown <zz240614@gmail.com>
2025-03-14 18:38:03 +0000

    ajout de l'interface utilisateur pour App de Message le concept de inteface une zone aficher la discusion beetween user et une zone de recherche et des indicateur de status online avec integration de menu de user avec option de profil et deconnxion avec zone de afficher des message sous form de bulls avec horodatage and devloper UI pourrr manific experince utilisateur

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('login');
 });
+Route::get('/chat', function () {
+    return view('chat.chat');
+})->name('chat.chat');
 
 
 
