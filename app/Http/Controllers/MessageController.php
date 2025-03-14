5a4082e3eb0197e293a39ca4257dc0ebab70b9cf
istifano <elamiriamir7@gmail.com>
2025-03-14 11:59:51 +0100

    j ai criee les routes dapplication avec la methode chat(qui affiche les messages entre les deux  utilisateurs ) et envoyer messaeges (qui criee un Events SendMessage )

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSend;
 use App\Models\User;
+use App\Models\Message;
+
 use Illuminate\Http\Request;
 use Illuminate\Container\Attributes\Auth;
 
@@ -13,4 +16,33 @@ class MessageController extends Controller
         
 return view("users",compact(["users"=>$users]));
     }
+
+    public function chat(Request $request){
+$receiver_id =$request->receiver_id;
+$user = User::find($request->receiver_id);
+
+$message = Message::where(function ($query) use ($receiver_id) {
+$query->where("sender_id",'=',Auth::id())->where("receiver_id",'=',$receiver_id);
+
+     })->orWhere(function ($query) use ($receiver_id){
+
+        $query->where("sender_id",'=',$receiver_id)->where("receiver_id",'=',Auth::id());
+
+     });
+     return view('', compact('receiver', 'messages'));
+
+    }
+    public function sendMessage(Request $request){
+        $message = Message::create([
+            'sender_id'     => Auth::id(),
+            'receiver_id'   => $request->receiverId,
+            'message'       => $request['message']
+        ]);
+
+        broadcast(new MessageSend($message))->toOthers();
+        
+        return response()->json(['status' => 'Message sent succ']);
+
+    }
+    
 }
